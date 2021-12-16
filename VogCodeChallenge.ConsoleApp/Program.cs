4ea4750f131d2781f5a21ebcbf0dd6dca7d67111
dhavalpithva <dhavalpithva05@gmail.com>
2021-12-16 22:47:28 +0530

    question_7
    Added a console project to the solution. Iterated through the NamesList items without using ForEach/For loops

---
+++ VogCodeChallenge.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           QuestionClass.NamesList.ForEach((x) => Console.WriteLine("Name = {0}", x));
+        }
+    }
+}
