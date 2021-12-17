678065b4853dd0813cba9b93901610968bb680cc
dhavalpithva <dhavalpithva05@gmail.com>
2021-12-17 09:18:22 +0530

    question_8_final
    Create a method called TESTModule, this method simply is a method that we pass values to it and it returns the result.
    With one Switch Statement, cover the provided requirements.

--- VogCodeChallenge.ConsoleApp/Program.cs
+++ VogCodeChallenge.ConsoleApp/Program.cs
@@ -6,7 +6,43 @@ namespace VogCodeChallenge.ConsoleApp
     {
         static void Main(string[] args)
         {
-           QuestionClass.NamesList.ForEach((x) => Console.WriteLine("Name = {0}", x));
+            QuestionClass.NamesList.ForEach((x) => Console.WriteLine("Name = {0}", x));
+            
+            Console.WriteLine("Case A: " + TESTModule(1));
+            Console.WriteLine("Case B: " + TESTModule(7));
+            Console.WriteLine("Case C: " + TESTModule(-1));
+            Console.WriteLine("Case D: " + TESTModule(1.0f));
+            Console.WriteLine("Case E: " + TESTModule("Hello"));
+        }
+
+
+        static object TESTModule(object obj)
+        {
+            object value;
+            switch (obj)
+            {
+                case int n when (n >= 1 && n <= 4):
+                    value = n * 2;
+                    break;
+                case int n when (n > 4):
+                    value = n * 3;
+                    break;
+                case int n when (n < 1):
+                    value = new ArithmeticException("Provided Value is Zero/Negative");
+                    break;
+                case float d when (d == 1.0f || d == 2.0f):
+                    value = 3.0f;
+                    break;
+                case string s:
+                    value = s.ToUpper();
+                    break;
+                default:
+                    value = obj;
+                    break;
+            }
+
+            return value;
+
         }
     }
 }
